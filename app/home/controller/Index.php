0eb61c599f9cc1d5c06724ca55dd7e817b940e18
liucunzhou <liucunzhou@163.com>
2020-05-20 09:13:42 +0800

    '报名'

--- app/home/controller/Index.php
+++ app/home/controller/Index.php
@@ -3,10 +3,17 @@ declare (strict_types = 1);
 
 namespace app\home\controller;
 
+use think\facade\View;
+
 class Index
 {
     public function index()
     {
         return redirect('/admin/passport/login');
+    }
+
+    public function teacher()
+    {
+        return View::fetch();
     }
 }
