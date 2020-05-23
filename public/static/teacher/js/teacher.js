8c480767cf546c096328178ba604a1bf864e33b1
liucunzhou <liucunzhou@163.com>
2020-05-23 22:36:48 +0800

    'cropper'

--- public/static/teacher/js/teacher.js
+++ public/static/teacher/js/teacher.js
@@ -54,5 +54,103 @@ $(function () {
             }
         });
     }
+
+    $("#preview").click(function () {
+        var name = $("#name").val();
+        var position = $("#position").val();
+        preview(name, position);
+    });
+
+    preview();
+
+    function preview(name='', position='') {
+
+        var bg = {
+            width: 750,
+            height: 750,
+            src: "/static/teacher/img/bg.png"
+        }
+
+        var logo = {
+            width: 233,
+            height: 56,
+            src: "/static/teacher/img/logo.png"
+        }
+
+        var celebrate = {
+            width: 220,
+            height: 233,
+            src: "/static/teacher/img/celebrate.png"
+        }
+
+        var flag = {
+            width: 683,
+            height: 182,
+            src: "/static/teacher/img/huizhang.png"
+        }
+
+        var teacher = {
+            width: 562,
+            height: 562,
+            src: "/static/teacher/img/teacher.png"
+        }
+
+        var imgBg = new Image();
+        var imgLogo = new Image();
+        var imgCelebrate = new Image();
+        // var imgTeacher = new Image();
+        var imgFlag = new Image();
+
+        imgBg.src = bg.src;
+        imgLogo.src = logo.src;
+        imgCelebrate.src = celebrate.src;
+        // imgTeacher.src = teacher.src;
+        imgFlag.src = flag.src;
+
+        var imgTeacher = new Image();
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+
+        imgBg.onload = function () {
+            ctx.drawImage(imgBg, 0, 0, bg.width, bg.height);
+            // ctx.save();
+            // 生成老师背景
+            // 红色背景
+            ctx.fillStyle = "#ba1200";
+            ctx.beginPath();
+            ctx.arc(375, 375, 281, 0, Math.PI * 2, false);
+            ctx.closePath();
+            ctx.fill();
+            // 青色背景
+            ctx.fillStyle = "#e6e6e6";
+            ctx.beginPath();
+            ctx.arc(375, 375, 274, 0, Math.PI * 2, false);
+            ctx.closePath();
+            ctx.fill();
+
+            ctx.drawImage(imgLogo, 38, 38, logo.width, logo.height);
+            ctx.drawImage(imgCelebrate, 472, 38, celebrate.width, celebrate.height);
+            // ctx.drawImage(imgTeacher, 94, 94, teacher.width + 14, imgTeacher.height + 14);
+            ctx.drawImage(imgFlag, 36, 568, imgFlag.width, imgFlag.height);
+
+            ctx.font = "55px Microsoft YaHei";
+            ctx.fillStyle = '#1a1a1a';
+            var x = 375 - 55 * name.length - 15;
+            ctx.fillText(name, x, 652);
+            ctx.font = "33px Microsoft YaHei";
+            ctx.fillText(position, 375, 652);
+            ctx.save();
+
+            var mainImg = new Image();
+            var imgSrc = canvas.toDataURL('image/png')
+            mainImg.src = imgSrc;
+            $('#preview-box').html("");//移除已生成的避免重复生成
+            $('#preview-box').append(mainImg);
+        }
+    }
+    
+    $("#upload").click(function () {
+        $("#avatar-modal").show();
+    });
 })
 
